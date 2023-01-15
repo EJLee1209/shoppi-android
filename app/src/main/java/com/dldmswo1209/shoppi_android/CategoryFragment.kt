d4ff8a88e418d991b50e9daf9199d351b5992754
EJLee1209 <dldmswo1209@gmail.com>
2023-01-15 18:09:40 +0900

    Navigation으로 화면전환 하기

--- app/src/main/java/com/dldmswo1209/shoppi_android/CategoryFragment.kt
+++ app/src/main/java/com/dldmswo1209/shoppi_android/CategoryFragment.kt
@@ -4,27 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 
-class HomeFragment: Fragment() {
-
+class CategoryFragment: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_category, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btn = view.findViewById<Button>(R.id.btn_enter_product_detail)
-
-        btn.setOnClickListener {
-            findNavController().navigate(R.id.action_home_to_product_detail)
-        }
-
     }
 }
